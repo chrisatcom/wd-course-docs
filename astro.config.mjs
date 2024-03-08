3e3a2f610f78b58361f5a26a38862f090fec747e
Chris Carpenter <ccarpenter@com.edu>
2024-03-08 14:28:49 -0600

    Changed shiki theme and tab spaces for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+// Current preferred theme: 'dracula-soft'
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme-darker',
+    }
+  }
+});
